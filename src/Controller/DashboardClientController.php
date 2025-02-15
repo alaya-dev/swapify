691e1d61310a56e5c9bfdd9fa5dc8601e2f27368
dali-97 <baarounmed3@gmail.com>
2025-02-15 04:47:25 +0100

    fix the session

--- src/Controller/DashboardClientController.php
+++ src/Controller/DashboardClientController.php
@@ -3,14 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DashboardClientController extends AbstractController
 {
     #[Route('/dashboard/client', name: 'app_dashboard_client')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // remove this line to it juste for check if teh sesstion is working wassim raged m3a raha <3 hh 
+        $session = $request->getSession();
+        $session->set('test_key', 'test_value');
+        dd($session);
+        return new Response('Session test: ' . $session->get('test_key'));
+
         return $this->render('dashboard/accueil_client.html.twig', [
             'controller_name' => 'DashboardClientController',
         ]);
