10860d1ba854a6c5957bf60369115bf7e6333b35
dali-97 <baarounmed3@gmail.com>
2025-02-15 16:41:53 +0100

    fix some problem

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -5,19 +5,14 @@ namespace App\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use App\Entity\User;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
 
 class AppAuthenticator extends AbstractLoginFormAuthenticator
@@ -26,14 +21,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    private UrlGeneratorInterface $urlGenerator;
-    private UserRepository $userRepository;
-
-    public function __construct(UrlGeneratorInterface $urlGenerator, UserRepository $userRepository)
-    {
-        $this->urlGenerator = $urlGenerator;
-        $this->userRepository = $userRepository;
-    }
+    public function __construct(private  UrlGeneratorInterface $urlGenerator, private UserRepository $userRepository) {}
 
     public function authenticate(Request $request): Passport
     {
@@ -71,8 +59,6 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($this->urlGenerator->generate('app_dashboard_client'));
         }
     }
-
-
 
     protected function getLoginUrl(Request $request): string
     {
