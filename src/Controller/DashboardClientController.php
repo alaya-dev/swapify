310bd66f0ce01cf63b8e32adcd3204325190e8ec
dali-97 <baarounmed3@gmail.com>
2025-02-15 03:40:52 +0100

    adding dashbord admine and taillwind config

--- src/Controller/DashboardClientController.php
+++ src/Controller/DashboardClientController.php
@@ -16,5 +16,40 @@ class DashboardClientController extends AbstractController
         ]);
     }
 
-    
+
+    #[Route('/dashboard/client/souk', name: 'souk')]
+    public function souk(): Response
+    {
+        return $this->render('dashboard/souk.html.twig');
+    }
+
+    #[Route('/dashboard/client/annonce', name: 'annonce')]
+    public function annonce(): Response
+    {
+        return $this->render('dashboard/annonce.html.twig');
+    }
+
+    #[Route('/dashboard/client/blog', name: 'blog')]
+    public function blog(): Response
+    {
+        return $this->render('dashboard/blog.html.twig');
+    }
+
+    #[Route('/dashboard/client/evenment', name: 'evenment')]
+    public function evenment(): Response
+    {
+        return $this->render('dashboard/evenment.html.twig');
+    }
+
+    #[Route('/dashboard/client/livraison', name: 'livraison')]
+    public function livraison(): Response
+    {
+        return $this->render('dashboard/livraison.html.twig');
+    }
+
+    #[Route('/dashboard/client/conversation', name: 'conversation')]
+    public function conversation(): Response
+    {
+        return $this->render('dashboard/conversation.html.twig');
+    }
 }
